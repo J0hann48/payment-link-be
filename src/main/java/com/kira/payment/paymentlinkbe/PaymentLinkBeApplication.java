42ad7481dc902cd387ec3ea3aba0ff6b258da1e4
Johann Torres <joahnn.1996@gmail.com>
2025-11-15 22:48:29 -0500

    first commit

---
+++ src/main/java/com/kira/payment/paymentlinkbe/PaymentLinkBeApplication.java
@@ -0,0 +1,13 @@
+package com.kira.payment.paymentlinkbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentLinkBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentLinkBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kira/payment/paymentlinkbe/PaymentLinkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kira.payment.paymentlinkbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentLinkBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
